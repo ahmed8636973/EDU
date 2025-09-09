cc0df2a7bd7768cf14e8ca44d46406b22ade8e6f
ahmed8636973 <125884417+ahmed8636973@users.noreply.github.com>
2025-09-09 10:30:54 -0700

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import AdminDashboard from "./pages/AdminDashboard";
 import StudentDashboard from "./pages/StudentDashboard";
+import { Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/admindashboard" element={<AdminDashboard />} />
-        <Route path="/studentdashboard" element={<StudentDashboard />} />
-        {/* fallback لو دخل user على رابط غلط */}
-        <Route path="*" element={<Home />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/admindashboard" element={<AdminDashboard />} />
+      <Route path="/studentdashboard" element={<StudentDashboard />} />
+      {/* fallback لو حد دخل مسار غلط */}
+      <Route path="*" element={<Home />} />
+    </Routes>
   );
 }
