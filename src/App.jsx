316f3f24ac6292f1dcdd72161ae7ba259a043b1d
ahmed8636973 <125884417+ahmed8636973@users.noreply.github.com>
2025-09-09 10:16:30 -0700

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
-import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
+import AdminDashboard from "./pages/AdminDashboard";
+import StudentDashboard from "./pages/StudentDashboard";
 
-export default function App() {
+function App() {
   return (
-    <div>
-      <Navbar />
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/student" element={<StudentDashboard />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-    </div>
+    </Router>
   );
 }
+
+export default App;
