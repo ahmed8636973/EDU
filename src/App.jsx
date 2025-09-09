cfa1bf0e826b939db3e5836880f7bcc8a89f8a19
ahmed8636973 <125884417+ahmed8636973@users.noreply.github.com>
2025-09-09 10:55:06 -0700

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import AdminDashboard from "./pages/AdminDashboard";
 import StudentDashboard from "./pages/StudentDashboard";
-import { Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
@@ -12,8 +12,7 @@ export default function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/admindashboard" element={<AdminDashboard />} />
       <Route path="/studentdashboard" element={<StudentDashboard />} />
-      {/* fallback لو حد دخل مسار غلط */}
-      <Route path="*" element={<Home />} />
+      <Route path="*" element={<Home />} /> {/* fallback */}
     </Routes>
   );
 }
