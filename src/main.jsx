5c37796636f903406b75e51df671c7224f9dd3c0
ahmed8636973 <125884417+ahmed8636973@users.noreply.github.com>
2025-09-09 10:30:19 -0700

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import AdminDashboard from "./pages/AdminDashboard";
-import StudentDashboard from "./pages/StudentDashboard";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/admindashboard" element={<AdminDashboard />} />
-        <Route path="/studentdashboard" element={<StudentDashboard />} />
-        {/* fallback لو حد دخل مسار غلط */}
-        <Route path="*" element={<Home />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
